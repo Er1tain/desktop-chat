5969506a378b9d9aa982ba63014f5a15c37fd10f
Er1tain <sanyapridava@mail.ru>
2024-11-24 15:02:36 +0300

    fix: now method set_nick destroy frame

--- ui/set_nick.py
+++ ui/set_nick.py
@@ -1,9 +1,10 @@
-from tkinter import messagebox, Tk
+from tkinter import messagebox, Tk, Frame
 
-def set_nick(str_nick: str) -> None:
+def set_nick(root: Tk, frame_input_nick: Frame, str_nick: str) -> None:
     if len(str_nick) == 0:
         messagebox.showerror(message="Ник обязателен!")
         return
     
-
+    # Закрытие фрейма для ввода ника
+    frame_input_nick.destroy()
     
