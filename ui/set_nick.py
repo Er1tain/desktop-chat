f2e32a529d13ad6c22a6eca8cea12e0d7264afbe
Er1tain <sanyapridava@mail.ru>
2024-11-24 16:28:45 +0300

    feat: added render frame of messanger

--- ui/set_nick.py
+++ ui/set_nick.py
@@ -1,4 +1,5 @@
 from tkinter import messagebox, Tk, Frame
+from widgets.GroupChatFrame import GroupChatFrame
 
 def set_nick(root: Tk, frame_input_nick: Frame, str_nick: str) -> None:
     if len(str_nick) == 0:
@@ -8,3 +9,6 @@ def set_nick(root: Tk, frame_input_nick: Frame, str_nick: str) -> None:
     # Закрытие фрейма для ввода ника
     frame_input_nick.destroy()
     
+    # Открытие общего чата
+    group_chat_frame = GroupChatFrame(root)
+    group_chat_frame.place(x=35, y=20)
